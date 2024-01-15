5ae70e3e6d962a29cf13d9593de3cfdc9f07dd05
Brian Mawira <brian.mawi@yahoo.com>
2024-01-15 22:01:34 +0300

    function to fetch api health status

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,13 +11,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String message = "Server health unknown yet!";
+  
+  // method to fetch api health using http package
+  Future loadServerHealth() async {
+    try {
+      final response =
+          await http.get(Uri.parse("http://10.0.2.2:3000/api/health"));
+
+      var jsonData = json.decode(response.body);
+
+      setState(() {
+        message = jsonData.message;
+      });
+    } catch (err) {
+      throw Exception(err);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('All users')),
-        body: const Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [],
-        ));
+      appBar: AppBar(
+        title: const Text('All users'),
+        backgroundColor: Colors.white,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(message),
+          Expanded(child: Container()),
+          TextButton(
+            onPressed: loadServerHealth,
+            child: const Text("Load Server Health"),
+          )
+        ],
+      ),
+    );
   }
 }
